f9274eb56be7f3396a0b9d62c6b7a2dee3d3b1f5
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-11-05 02:41:09 -0500

    Add nodemon, express and milligram

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
+import { Routes, RouterModule } from '@angular/router';
+
+import { GameComponent } from './game/game.component';
+
+const appRoutes: Routes = [
+  { path: '', component: GameComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
